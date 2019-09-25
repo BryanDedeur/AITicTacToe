ecef0514f59622ef97c72e9a62dabd7d75404ab1
BryanDedeur <bdedeurwaerder@nevada.unr.edu>
2019-09-24 22:23:39 -0700

    release minmax strategy

--- TicTacToe/Assets/Scripts/Board.cs
+++ TicTacToe/Assets/Scripts/Board.cs
@@ -6,68 +6,51 @@ using Unity.Collections;
 using Unity.Jobs;
 using UnityEngine;
 
-public class Board : MonoBehaviour
+public static class Board
 {
-    //public string encoded;
-    public short numAvailableMoves;
-    public char lastPlayer;
-    public char winner;
-    public short weight;
+    public static char emptyPosition = '_';
 
-    public ushort lastMoveX;
-    public ushort lastMoveY;
-    public char[,] state;
-    public char emptyPosition = '_';
-
-    // Start is called before the first frame update
-    public void Initialize()
+    public struct move
     {
-        lastPlayer = emptyPosition;
-        numAvailableMoves = 9;
-        winner = emptyPosition;
-        state = new char[3, 3];
+        public short score;
+        public ushort depth;
+        //public char player;
+        public ushort x;
+        public ushort y;
+    }
+
+    public static string Encode(char[,] board)
+    {
+        string encodedBoard = "";
         for (ushort x = 0; x < 3; x++)
         {
             for (ushort y = 0; y < 3; y++)
             {
-                state[x, y] = emptyPosition;
+                encodedBoard += board[x, y];
             }
         }
+        return encodedBoard;
     }
 
-    public char OppositePlayer()
+    public static char OppositePlayer(char player)
     {
-        if (lastPlayer == 'X')
+        if (player == 'X')
         {
             return 'O';
         }
         return 'X';
     }
     
-    public bool IsValidMove(char player, ushort x, ushort y)
+    public static bool IsValidMove(char[,] board, char player, ushort x, ushort y)
     {
-        if (player == lastPlayer)
-            return false;
         if (!(x < 3 && y < 3))
             return false;
-        if (state[x, y] != emptyPosition)
+        if (board[x, y] != emptyPosition)
             return false;
         return true;
     }
-
-    public bool MakeMove(char player, ushort x, ushort y)
-    {
-        if (IsValidMove(player, x, y))
-        {
-            numAvailableMoves -= 1;
-            lastPlayer = player;
-            state[x, y] = player;
-            return true;
-        }
-        return false;
-    }
-
-    public string Print()
+    
+    public static string Print(char[,] board)
     {
         string output = "";
         for (ushort x = 0; x < 3; x++)
@@ -75,103 +58,133 @@ public class Board : MonoBehaviour
             string line = "";
             for (ushort y = 0; y < 3; y++)
             {
-                line += state[y, x];
+                line += board[y, x];
             }
             output += line + '\n';
         }
         return output;
     }
 
-    public void MirorBoard(Board passedBoard)
+    public static char[,] MirorBoard(char[,] board)
     {
+        char[,] newBoard = new char[3,3];
         for (ushort x = 0; x < 3; x++)
         {
             for (ushort y = 0; y < 3; y++)
             {
-                state[x, y] = passedBoard.state[x,y];
+                newBoard[x, y] = board[x,y];
             }
         }
-        lastPlayer = passedBoard.lastPlayer;
-        numAvailableMoves = passedBoard.numAvailableMoves;
-        lastMoveX = passedBoard.lastMoveX;
-        lastMoveY = passedBoard.lastMoveY;
-        winner = passedBoard.winner;
+        return newBoard;
     }
 
-    public char CheckForWin()
+    public static short AvailableMovesCount(char[,] board)
     {
-        // not possible to win with only 5 moves on board
-        if (numAvailableMoves <= 5)
-            return emptyPosition;
+        short counter = 0;
+        for (ushort x = 0; x < 3; x++)
+        {
+            for (ushort y = 0; y < 3; y++)
+            {
+                if (board[x, y] == emptyPosition)
+                {
+                    counter += 1;
+                }
+            }
+        }
+
+        return counter;
+    }
+    
+
+    // checks the current move and returns true or false if win detected
+    public static bool CheckForWin(char[,] board, char player, ushort moveX, ushort moveY)
+    {
+        // not possible to win with only 4 moves on board
+        if (AvailableMovesCount(board) > 5) 
+            return false;
     
         // check win scenarios only on verticle and horizontal line of current move
         ushort count = 0;
         // vertical to current move
         for (ushort y = 0; y < 3; y++)
-            if (state[lastMoveX, y] == lastPlayer)
+            if (board[moveX, y] == player)
                 count += 1;
             else
                 break;
 
         if (count > 2)
-            return lastPlayer;
+            return true;
         else
             count = 0;
     
         // horizontal to current move
         for (ushort x = 0; x < 3; x++)
-            if (state[x, lastMoveY] == lastPlayer)
+            if (board[x, moveY] == player)
                 count += 1;
             else
                 break;
     
         if (count > 2)
-            return lastPlayer;
+            return true;
 
         // diagonals if move is in diagonal space
-        if (Mathf.Abs(lastMoveX - lastMoveY) == 0 || Mathf.Abs(lastMoveX - lastMoveY) == 2)
+        if (Mathf.Abs(moveX - moveY) == 0 || Mathf.Abs(moveX - moveY) == 2)
         {
-            if (state[0, 0] == lastPlayer && state[1, 1] == lastPlayer && state[2, 2] == lastPlayer)
-                return lastPlayer;
-            if (state[2, 0] == lastPlayer && state[1, 1] == lastPlayer && state[0, 2] == lastPlayer)
-                return lastPlayer;
+            if (board[0, 0] == player && board[1, 1] == player && board[2, 2] == player)
+                return true;
+            if (board[2, 0] == player && board[1, 1] == player && board[0, 2] == player)
+                return true;
         }
-    
-        return emptyPosition;
-        
+        return false;
     }
 
-    public bool TerminalState()
+    public static char GetWinner(char[,] board, char player, ushort moveX, ushort moveY)
     {
-        if (numAvailableMoves == 0)
+        ushort count = 0;
+        for (ushort y = 0; y < 3; y++)
+            if (board[moveX, y] == player)
+                count += 1;
+            else
+                break;
+
+        if (count > 2)
+            return player;
+        else
+            count = 0;
+    
+        for (ushort x = 0; x < 3; x++)
+            if (board[x, moveY] == player)
+                count += 1;
+            else
+                break;
+    
+        if (count > 2)
+            return player;
+
+        if (Mathf.Abs(moveX - moveY) == 0 || Mathf.Abs(moveX - moveY) == 2)
         {
-            weight = 0;
+            if (board[0, 0] == player && board[1, 1] == player && board[2, 2] == player)
+                return player;
+            if (board[2, 0] == player && board[1, 1] == player && board[0, 2] == player)
+                return player;
+        }
+        return emptyPosition;
+    }
+
+
+
+    public static bool TerminalState(char[,] board, char player, ushort moveX, ushort moveY )
+    {
+        if (AvailableMovesCount(board) == 0)
+        {
             return true;
         }
 
-        winner = CheckForWin();
-        if (winner != emptyPosition)
+        if (CheckForWin(board, player, moveX, moveY))
         {
-            if (winner == 'X')
-            {
-                weight = 1;
-            }
-            else
-            {
-                weight = -1;
-            }
             return true;
         }
 
         return false;
-    }
-    
-    private bool IsMovesLeft()
-    {
-        if (numAvailableMoves == 0)
-        {
-            return false;
-        }
-        return true;
     }
 }
--- TicTacToe/Assets/Scripts/MinMaxAlgorithm.cs
+++ TicTacToe/Assets/Scripts/MinMaxAlgorithm.cs
@@ -1,63 +1,127 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using ProBuilder2.Common;
+﻿using System.Collections.Generic;
 using UnityEngine;
+
 
 public class MinMaxAlgorithm : MonoBehaviour
 {
     // public 
-    public TicTacToe ttt;
-    public StateTree stateTree;
-    public bool treeComputed = false;
+    private Dictionary<string, Board.move> scenarioMap; // string = encoded board state, move = next best move
 
-    public void Awake()
-    {
-        stateTree = transform.gameObject.AddComponent<StateTree>();
-    }
+    public char maximizingPlayer = 'X';
+    // private
 
-    public void Initialize()
+    public Board.move FindBestMove(char[,] board, char player)
     {
-        treeComputed = true;
-        stateTree.MakeStateTree(ttt.gameBoard);
-    }
-
-    public Board minmax(Board board, char player)
-    {
-        if (board.TerminalState())
+        Board.move bestMove = new Board.move();
+        bestMove.score = -100;
+        for (ushort x = 0; x < 3; x++)
         {
-            return board;
-        }
-
-        if (player == 'X')
-        {
-            Board bestBoard = new Board();
-            short bestWeight = -100;
-            for (int i = 0; i < stateTree.scenarioBoards.Length; i++)
+            for (ushort y = 0; y < 3; y++)
             {
-                Board foundBoard = minmax(stateTree.scenarioBoards[i], board.OppositePlayer());
-                if (bestWeight > foundBoard.weight)
+                if (board[x, y] == Board.emptyPosition)
                 {
-                    bestBoard = foundBoard;
-                    bestWeight = foundBoard.weight;
+                    char[,] scenarioBoard = Board.MirorBoard(board);
+                    scenarioBoard[x, y] = player;
+                    string encodedBoard = Board.Encode(board);
+                    // using memoization to speed things up
+                    if (scenarioMap.ContainsKey(encodedBoard))
+                    {
+                        if (scenarioMap[encodedBoard].score > bestMove.score)
+                        {
+                            bestMove = scenarioMap[encodedBoard];
+                        }
+                    }
+                    else
+                    {
+                        if (Minimax(scenarioBoard, 0, player, x, y) > bestMove.score)
+                        {
+                            bestMove = scenarioMap[encodedBoard];
+                        }
+                    }
                 }
             }
-            return bestBoard;
-        } else
+        }
+        return bestMove;
+    }
+    
+    public short GetBoardScore(char[,] board, char player, ushort x, ushort y)
+    {
+        char winner = Board.GetWinner(board, player, x, y);
+        if (winner == maximizingPlayer)
         {
-            Board bestBoard = new Board();
-            short bestWeight = -100;
-            for (int i = 0; i < stateTree.scenarioBoards.Length; i++)
+            return -10;
+        }
+        else if (winner == Board.OppositePlayer(maximizingPlayer))
+        {
+            return 10;
+        }
+
+        return 0;
+    }
+    
+    public short Minimax(char[,] board, ushort depth, char player, ushort x, ushort y)
+    {
+        Board.move someMove = new Board.move();
+        someMove.depth = depth;
+        someMove.x = x;
+        someMove.y = y;
+        string encodedBoard = Board.Encode(board);
+        if (Board.TerminalState(board, player, x, y))
+        {
+            someMove.score = GetBoardScore(board, player, x, y);
+            scenarioMap.Add(encodedBoard, someMove);
+            return someMove.score;
+        }
+        
+        Board.move bestMove = new Board.move();
+        depth += 1;
+        if (player == maximizingPlayer) // maximize
+        {
+            bestMove.score = -100;
+            for (ushort xIndex = 0; xIndex < 3; xIndex++)
             {
-                Board foundBoard = minmax(stateTree.scenarioBoards[i], board.OppositePlayer());
-                if (bestWeight > foundBoard.weight)
+                for (ushort yIndex = 0; yIndex < 3; yIndex++)
                 {
-                    bestBoard = foundBoard;
-                    bestWeight = foundBoard.weight;
+                    if (board[xIndex, yIndex] == Board.emptyPosition)
+                    {
+                        if (!scenarioMap.ContainsKey(encodedBoard))
+                        {
+                            board[xIndex, yIndex] = Board.OppositePlayer(player);
+                            someMove.score = Minimax(board, depth, Board.OppositePlayer(player), xIndex, yIndex);
+                            scenarioMap.Add(encodedBoard, someMove);
+                        }
+                        if (someMove.score > bestMove.score)
+                        {
+                            someMove = scenarioMap[encodedBoard];
+                        }
+                    }
                 }
             }
-            return bestBoard;
         }
+        else // minimize
+        {
+            bestMove.score = 100;
+            for (ushort xIndex = 0; xIndex < 3; xIndex++)
+            {
+                for (ushort yIndex = 0; yIndex < 3; yIndex++)
+                {
+                    if (board[xIndex, yIndex] == Board.emptyPosition)
+                    {
+                        if (!scenarioMap.ContainsKey(encodedBoard))
+                        {
+                            board[xIndex, yIndex] = Board.OppositePlayer(player);
+                            someMove.score = Minimax(board, depth, Board.OppositePlayer(player), xIndex, yIndex);
+                            scenarioMap.Add(encodedBoard, someMove);
+                        }
+                        if (someMove.score < bestMove.score)
+                        {
+                            someMove = scenarioMap[encodedBoard];
+                        }
+                    }
+                }
+            }
+        }
+        
+        return someMove.score;
     }
 }
